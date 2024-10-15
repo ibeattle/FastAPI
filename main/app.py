dd73a1ad23f2d2b4364454f23cd74fd0c32d02f7
ibeattle <59359604+ibeattle@users.noreply.github.com>
2024-10-15 22:03:44 +0300

    Update app.py

--- main/app.py
+++ main/app.py
@@ -7,12 +7,13 @@ from sqlalchemy import select, insert, update, and_
 
 def create_app(test_config=None):
     if test_config is None:
-        db_name = 'sqlite:///parking.db'
+        db_name = "sqlite:///parking.db"
     else:
-        db_name = 'sqlite:///test.db'
+        db_name = "sqlite:///test.db"
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = db_name
     from .models import db
+
     db.init_app(app)
 
     @app.before_request
@@ -29,7 +30,7 @@ def create_app(test_config=None):
 def create_clients_endpoints(app):
     from .models import db, Client, Parking
 
-    @app.route('/clients')
+    @app.route("/clients")
     def all_clients():
         result = db.session.execute(select(Client)).scalars().all()
         clients_list = []
@@ -37,47 +38,57 @@ def create_clients_endpoints(app):
             clients_list.append(client.to_json())
         return clients_list
 
-    @app.route('/clients', methods=['POST'])
+    @app.route("/clients", methods=["POST"])
     def add_client():
         data = request.json
         result = db.session.execute(
             insert(Client)
             .values(
-                name=data['name'],
-                surname=data['surname'],
+                name=data["name"],
+                surname=data["surname"],
                 # credit_card=data['credit_card'],
                 # car_number=data['car_number']
             )
-            .returning(
-                Client.id, Client.name, Client.surname
-            )
+            .returning(Client.id, Client.name, Client.surname)
         ).fetchone()
         db.session.commit()
         return {"id": result[0], "name": result[1], "surname": result[2]}, 201
 
-    @app.route('/clients/<int:client_id>')
+    @app.route("/clients/<int:client_id>")
     def client_by_id(client_id):
-        result = db.session.execute(select(Client).where(Client.id == client_id)).scalars().first()
+        result = (
+            db.session.execute(select(Client).where(Client.id == client_id))
+            .scalars()
+            .first()
+        )
         if result:
             return result.to_json()
         else:
-            return {'not_found': 404}
+            return {"not_found": 404}
 
     @app.route("/parkings", methods=["POST"])
     def add_parking_zone():
         data = request.json
         result = db.session.execute(
-            insert(Parking).values(
-                address=data['address'],
-                count_places=data['count_places'],
-                count_available_places=data['count_available_places'],
-            ).returning(
-                Parking.id, Parking.address, Parking.count_places, Parking.count_available_places
+            insert(Parking)
+            .values(
+                address=data["address"],
+                count_places=data["count_places"],
+                count_available_places=data["count_available_places"],
+            )
+            .returning(
+                Parking.id,
+                Parking.address,
+                Parking.count_places,
+                Parking.count_available_places,
             )
         ).fetchone()
         db.session.commit()
         return {
-            "id": result[0], "address": result[1], "count_places": result[2], "count_available_places": result[3]
+            "id": result[0],
+            "address": result[1],
+            "count_places": result[2],
+            "count_available_places": result[3],
         }, 201
 
 
@@ -87,31 +98,56 @@ def create_parking_endpoints(app):
     @app.route("/client_parkings", methods=["POST"])
     def park():
         data = request.json
-        opened = db.session.execute(
-            select(Parking.opened).where(Parking.id == data['parking_id'])).scalars().first()
+        opened = (
+            db.session.execute(
+                select(Parking.opened).where(Parking.id == data["parking_id"])
+            )
+            .scalars()
+            .first()
+        )
         if opened:
-            result = db.session.execute(insert(ClientParking).values(
-                client_id=data['client_id'],
-                parking_id=data['parking_id'],
-                time_in=datetime.datetime.now()
-            ).returning(ClientParking.id, ClientParking.time_in)).fetchone()
+            result = db.session.execute(
+                insert(ClientParking)
+                .values(
+                    client_id=data["client_id"],
+                    parking_id=data["parking_id"],
+                    time_in=datetime.datetime.now(),
+                )
+                .returning(ClientParking.id, ClientParking.time_in)
+            ).fetchone()
 
-            count_available_places = db.session.execute(
-                select(Parking.count_available_places).where(Parking.id == data['parking_id'])
-            ).scalars().first()
+            count_available_places = (
+                db.session.execute(
+                    select(Parking.count_available_places).where(
+                        Parking.id == data["parking_id"]
+                    )
+                )
+                .scalars()
+                .first()
+            )
 
-            db.session.execute(update(Parking).values(count_available_places=count_available_places - 1))
+            db.session.execute(
+                update(Parking).values(
+                    count_available_places=count_available_places - 1
+                )
+            )
 
-            count_available_places = db.session.execute(
-                select(Parking.count_available_places).where(Parking.id == data['parking_id'])
-            ).scalars().first()
+            count_available_places = (
+                db.session.execute(
+                    select(Parking.count_available_places).where(
+                        Parking.id == data["parking_id"]
+                    )
+                )
+                .scalars()
+                .first()
+            )
 
             if count_available_places == 0:
                 db.session.execute(update(Parking).values(opened=False))
 
             db.session.commit()
             return {"id": result[0], "time_in": result[1]}, 201
-        return 'Все места заняты', 404
+        return "Все места заняты", 404
 
     @app.route("/client_parkings", methods=["DELETE"])
     def leave():
@@ -122,26 +158,54 @@ def create_parking_endpoints(app):
 
         data = request.json
         try:
-            client_parking_obj = db.session.execute(select(ClientParking).where(
-                and_(ClientParking.client_id == data['client_id'], ClientParking.parking_id == data['parking_id']))
-            ).scalars().first()
+            client_parking_obj = (
+                db.session.execute(
+                    select(ClientParking).where(
+                        and_(
+                            ClientParking.client_id == data["client_id"],
+                            ClientParking.parking_id == data["parking_id"],
+                        )
+                    )
+                )
+                .scalars()
+                .first()
+            )
             client_parking_obj.time_out = datetime.datetime.now()
             time_out = client_parking_obj.time_out
         except sqlalchemy.exc.NoResultFound:
             time_out = "NoResultFound"
 
-        count_available_places = db.session.execute(
-            select(Parking.count_available_places).where(Parking.id == data['parking_id'])
-        ).scalars().first()
-        db.session.execute(update(Parking).values(count_available_places=count_available_places + 1))
-        count_available_places = db.session.execute(
-            select(Parking.count_available_places).where(Parking.id == data['parking_id'])
-        ).scalars().first()
+        count_available_places = (
+            db.session.execute(
+                select(Parking.count_available_places).where(
+                    Parking.id == data["parking_id"]
+                )
+            )
+            .scalars()
+            .first()
+        )
+        db.session.execute(
+            update(Parking).values(count_available_places=count_available_places + 1)
+        )
+        count_available_places = (
+            db.session.execute(
+                select(Parking.count_available_places).where(
+                    Parking.id == data["parking_id"]
+                )
+            )
+            .scalars()
+            .first()
+        )
         if count_available_places > 0:
             db.session.execute(update(Parking).values(opened=True))
 
-        card = db.session.execute(
-            select(Client.credit_card).where(Client.id == data['client_id'])).scalars().first()
+        card = (
+            db.session.execute(
+                select(Client.credit_card).where(Client.id == data["client_id"])
+            )
+            .scalars()
+            .first()
+        )
         if card:
             payment = True
         else:
@@ -149,8 +213,8 @@ def create_parking_endpoints(app):
 
         db.session.commit()
         return {
-            "client_id": data['client_id'],
-            "parking_id": data['parking_id'],
+            "client_id": data["client_id"],
+            "parking_id": data["parking_id"],
             "time_out": time_out,
             "payment": payment,
         }
